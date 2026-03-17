14551d29faa16045d9ad096c91f482f1de4eec7d
HukaTuka <huiguihuiug@gmail.com>
2026-03-17 16:15:24 +0100

    Refactor IUser and UserDAO to use custom ExceptionHandler for improved error handling

--- Tickets_app/src/main/java/com/example/tickets_app/DAL/DAO/UserDAO.java
+++ Tickets_app/src/main/java/com/example/tickets_app/DAL/DAO/UserDAO.java
@@ -1,8 +1,13 @@
 package com.example.tickets_app.DAL.DAO;
 
 import com.example.tickets_app.BE.User;
+import com.example.tickets_app.BLL.util.ExceptionHandler;
+import com.example.tickets_app.DAL.DB.DBConnector;
 import com.example.tickets_app.DAL.Interface.IUser;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO implements IUser {
@@ -10,6 +15,21 @@ public class UserDAO implements IUser {
 
     @Override
     public void createUser(User user) {
+        String sql = "INSERT INTO Users (FirstName, LastName, Email, Phone, Password, Role) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getEmail());
+            ps.setString(4, user.getPhoneNumber());
+            ps.setString(5, user.getPassword());
+            ps.setString(6, user.getRole());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            ExceptionHandler.handleDAOException("createUser", e);
+        }
 
     }
 
@@ -29,7 +49,21 @@ public class UserDAO implements IUser {
     }
 
     @Override
-    public boolean emailExists(String email) throws SQLException {
+    public boolean emailExists(String email) throws ExceptionHandler {
+        String sql = "SELECT COUNT(*) FROM Users WHERE Email = ?";
+
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+
+        } catch (SQLException e) {
+            ExceptionHandler.handleDAOException("emailExists", e);
+        }
         return false;
     }
 }
--- Tickets_app/src/main/java/com/example/tickets_app/DAL/Interface/IUser.java
+++ Tickets_app/src/main/java/com/example/tickets_app/DAL/Interface/IUser.java
@@ -1,14 +1,13 @@
 package com.example.tickets_app.DAL.Interface;
 
 import com.example.tickets_app.BE.User;
-
-import java.sql.SQLException;
+import com.example.tickets_app.BLL.util.ExceptionHandler;
 
 public interface IUser {
-        void createUser(User user) throws SQLException;
-        void editUser(int userId) throws SQLException;
-        void deleteUser(int userId) throws SQLException;
-        boolean validateLogin(String username, String password) throws SQLException;
-        boolean emailExists(String email) throws SQLException;
+        void createUser(User user) throws ExceptionHandler;
+        void editUser(int userId) throws ExceptionHandler;
+        void deleteUser(int userId) throws ExceptionHandler;
+        boolean validateLogin(String username, String password) throws ExceptionHandler;
+        boolean emailExists(String email) throws ExceptionHandler;
 
 }
