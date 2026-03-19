247ece2636e7bbc5ded7ab1aa5a88e095c6976ab
Felix Runge <>
2026-03-19 10:24:00 +0100

    Add UserListCellController for managing user list item interactions

---
+++ Tickets_app/src/main/java/com/example/tickets_app/GUI/Controller/UserListCellController.java
@@ -0,0 +1,41 @@
+package com.example.tickets_app.GUI.Controller;
+
+import com.example.tickets_app.BE.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.util.function.Consumer;
+
+public class UserListCellController {
+
+    @FXML private Label lblUserInfo;
+    @FXML private Button btnEdit;
+    @FXML private Button btnDelete;
+
+    private Consumer<User> onDelete;
+    private Consumer<User> onEdit;
+    private User user;
+
+    public void setUser(User user, Consumer<User> onEdit, Consumer<User> onDelete) {
+        this.user = user;
+        this.onEdit = onEdit;
+        this.onDelete = onDelete;
+        lblUserInfo.setText(user.getFirstName() + " " + user.getLastName() + " (" + user.getRole() + ")");
+    }
+
+    @FXML
+    public void onDeleteClick(ActionEvent actionEvent) {
+        if (onDelete != null) {
+            onDelete.accept(user);
+        }
+    }
+
+    @FXML
+    public void onEditClick(ActionEvent actionEvent) {
+        if (onEdit != null) {
+            onEdit.accept(user);
+        }
+    }
+}
