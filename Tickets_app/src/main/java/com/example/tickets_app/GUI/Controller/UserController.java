f685c0871c01b00c1d1519b039d1606100e10a94
Felix Runge <>
2026-03-19 10:24:11 +0100

    Implement user list initialization and management in UserController

--- Tickets_app/src/main/java/com/example/tickets_app/GUI/Controller/UserController.java
+++ Tickets_app/src/main/java/com/example/tickets_app/GUI/Controller/UserController.java
@@ -1,20 +1,54 @@
 package com.example.tickets_app.GUI.Controller;
 
+import com.example.tickets_app.BE.User;
+import com.example.tickets_app.BLL.Interface.IUserManager;
+import com.example.tickets_app.BLL.UserManager;
+import com.example.tickets_app.BLL.util.ExceptionHandler;
+import com.example.tickets_app.DAL.DAO.UserDAO;
 import com.example.tickets_app.GUI.util.AlertUtil;
 import com.example.tickets_app.GUI.util.SceneUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class UserController {
 
-    @FXML
-    public void onEditUserClick(ActionEvent actionEvent) {
-        AlertUtil.showInfo("Edit user", "Edit user (demo action).");
-    }
+    @FXML private ListView<User> listViewUsers;
+
+    private final IUserManager userManager = new UserManager(new UserDAO());
+    private final ObservableList<User> userList = FXCollections.observableArrayList();
 
     @FXML
-    public void onDeleteUserClick(ActionEvent actionEvent) {
-        AlertUtil.showInfo("Delete user", "User has been deleted (demo action).");
+    public void initialize() {
+        listViewUsers.setItems(userList);
+        listViewUsers.setCellFactory(lv -> new UserListCell(this::handleEdit, this::handleDelete));
+        loadUsers();
+    }
+
+    private void loadUsers() {
+        try {
+            userList.setAll(userManager.getAllUsers());
+        } catch (ExceptionHandler e) {
+            AlertUtil.showError("Database error", "Could not load users: " + e.getMessage());
+        }
+    }
+
+    private void handleDelete(User user) {
+        if (AlertUtil.showConfirmation("Delete User", "Are you sure you want to delete " + user.getFirstName() + " " + user.getLastName() + "?")) {
+            try {
+                userManager.deleteUser(user.getId());
+                userList.remove(user);
+                AlertUtil.showInfo("User deleted", user.getFirstName() + " " + user.getLastName() + " has been deleted.");
+            } catch (ExceptionHandler e) {
+                AlertUtil.showError("Database error", e.getMessage());
+            }
+        }
+    }
+
+    private void handleEdit(User user) {
+        AlertUtil.showInfo("Edit user", "Edit functionality coming soon.");
     }
 
     @FXML
