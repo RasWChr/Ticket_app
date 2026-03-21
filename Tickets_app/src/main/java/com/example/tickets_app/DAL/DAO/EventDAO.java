217a17141bc6c91a75a38d320967d09f8011761d
HukaTuka <huiguihuiug@gmail.com>
2026-03-21 10:23:28 +0100

    Implement getAllEvents

--- Tickets_app/src/main/java/com/example/tickets_app/DAL/DAO/EventDAO.java
+++ Tickets_app/src/main/java/com/example/tickets_app/DAL/DAO/EventDAO.java
@@ -3,14 +3,42 @@ package com.example.tickets_app.DAL.DAO;
 import com.example.tickets_app.BE.Event;
 import com.example.tickets_app.BE.User;
 import com.example.tickets_app.BLL.util.ExceptionHandler;
+import com.example.tickets_app.DAL.DB.DBConnector;
 import com.example.tickets_app.DAL.Interface.IEventDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventDAO implements IEventDAO {
     @Override
     public List<Event> getAllEvents() throws ExceptionHandler {
-        return List.of();
+        String sql = "SELECT Id, Name, StartDateTime, EndDateTime, Location, LocationGuidance, Notes FROM Events";
+        List<Event> events = new ArrayList<>();
+
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                events.add(new Event(
+                        rs.getInt("Id"),
+                        rs.getString("Name"),
+                        rs.getString("StartDateTime"),
+                        rs.getString("EndDateTime"),
+                        rs.getString("Location"),
+                        rs.getString("LocationGuidance"),
+                        rs.getString("Notes")
+                ));
+            }
+
+        } catch (SQLException e) {
+            ExceptionHandler.handleDAOException("getAllEvents", e);
+        }
+        return events;
     }
 
     @Override
