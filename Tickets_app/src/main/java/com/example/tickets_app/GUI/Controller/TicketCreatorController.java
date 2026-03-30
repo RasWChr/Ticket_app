6fb036edb25d263bf76fda732a504a6bc4f67cad
Broccoli Leaf <r.west.chr@gmail.com>
2026-03-31 01:04:24 +0200

    IT WORKS

--- Tickets_app/src/main/java/com/example/tickets_app/GUI/Controller/TicketCreatorController.java
+++ Tickets_app/src/main/java/com/example/tickets_app/GUI/Controller/TicketCreatorController.java
@@ -2,6 +2,8 @@ package com.example.tickets_app.GUI.Controller;
 
 import com.example.tickets_app.BE.Event;
 import com.example.tickets_app.BE.Ticket;
+import com.example.tickets_app.BLL.EventManager;
+import com.example.tickets_app.BLL.Interface.IEventManager;
 import com.example.tickets_app.BLL.Interface.ITicketManager;
 import com.example.tickets_app.BLL.TicketManager;
 import com.example.tickets_app.DAL.DAO.EventDAO;
@@ -23,8 +25,49 @@ public class TicketCreatorController {
     @FXML private TextField txtDiscount;
     @FXML private TextField txtType;
 
+    private final static ITicketManager ticketManager = new TicketManager(new EventDAO(), new TicketDAO());
+    private Ticket ticketToEdit = null;
+
+    @FXML
+    public void initialize() {
+        List<Event> events = ticketManager.getAllEvents();
+        ObservableList<Event> eventList = FXCollections.observableArrayList(events);
+        cBoxEvent.setItems(eventList);
+    }
+
+    public void setTicketToEdit(Ticket ticket) {
+        this.ticketToEdit = ticket;
+
+        cBoxEvent.getItems().stream()
+                .filter(e -> ((Event) e).getId() == ticket.getEventID())
+                .findFirst()
+                .ifPresent(cBoxEvent::setValue);
+        txtType.setText(ticket.getTicketType());
+        txtPrice.setText(String.valueOf(ticket.getPrice()));
+        txtDiscount.setText(String.valueOf(ticket.getDiscount()));
+    }
 
     public void onCreateTicketClick(ActionEvent actionEvent) {
+        int eventId               = cBoxEvent.getValue() != null ? ((Event) cBoxEvent.getValue()).getId() : 0;
+        String ticketType         = txtType         != null ? txtType.getText()         : "";
+        double price              = Double.parseDouble(txtPrice != null ? txtPrice.getText() : "");
+        double discount           = Double.parseDouble(txtDiscount      != null ? txtDiscount.getText()      : "");
+
+
+        try {
+            if (ticketToEdit == null) {
+                ticketManager.createTicket(eventId, price, discount, ticketType);
+                AlertUtil.showInfo("Ticket saved", "Ticket \"" + eventId + ticketType + "\" has been created.");
+            } else {
+                ticketManager.editTicket(ticketToEdit.getId(), eventId, price, discount, ticketType);
+                AlertUtil.showInfo("Ticket updated", "Event \"" + eventId + ticketType + "\" has been updated.");
+            }
+            SceneUtil.switchScene(actionEvent, "Views/Main-Screen.fxml");
+        } catch (IllegalArgumentException e) {
+            AlertUtil.showWarning("Invalid input", e.getMessage());
+        } catch (Exception e) {
+            AlertUtil.showError("Database error", e.getMessage());
+        }
     }
 
     public void onBtnCancelClick(ActionEvent actionEvent) { SceneUtil.switchScene(actionEvent, "Views/Main-Screen.fxml");
