2dfeabaaedc89fd01b3feb351ec4c027a5be7fd5
Jon Lyngs Linnet <jonlin02@easv365.dk>
2026-04-02 00:40:28 +0200

    Ticket List Ticket List Cell TicketListCellController.java

---
+++ Tickets_app/src/main/java/com/example/tickets_app/GUI/Controller/TicketListCellController.java
@@ -0,0 +1,8 @@
+package com.example.tickets_app.GUI.Controller;
+
+import javafx.event.ActionEvent;
+
+public class TicketListCellController {
+    public void onOptionsClick(ActionEvent actionEvent) {
+    }
+}
